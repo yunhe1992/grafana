7f4985fc0030f081bd5bcfd4f0e7475f46b3d088
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2023-06-16 11:56:21 -0400

    example dashboard migration - currently failing validation

---
+++ dashmig/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/grafana/thema"
+	"github.com/grafana/thema/vmux"
+
+	"github.com/grafana/grafana/pkg/cuectx"
+	"github.com/grafana/grafana/pkg/kinds/dashboard"
+)
+
+func main() {
+	var inputFile, outputFile string
+	flag.StringVar(&inputFile, "in", "dashboard.json", "input json file to schematize")
+	flag.StringVar(&outputFile, "out", "", "output file (default: sdtout)")
+	flag.Parse()
+
+	src, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("opening input file failed: %s", err.Error())
+		os.Exit(1)
+	}
+
+	dashdata, err := vmux.NewJSONCodec(inputFile).Decode(cuectx.GrafanaCUEContext(), src)
+	if err != nil {
+		fmt.Printf("decoding input file failed: %s", err.Error())
+		os.Exit(1)
+	}
+
+	dk, err := dashboard.NewKind(cuectx.GrafanaThemaRuntime())
+	if err != nil {
+		fmt.Printf("creating dashboard kind failed: %s", err.Error())
+		os.Exit(1)
+	}
+
+	var i *thema.Instance
+	i = dk.Lineage().ValidateAny(dashdata)
+	if i == nil {
+		fmt.Println("ValidateAny returned a nil instance")
+		os.Exit(1)
+	}
+
+	if i.Schema().Version() != i.Schema().LatestInMajor().Version() {
+		i, _ = i.Translate(i.Schema().LatestInMajor().Version())
+	}
+
+	j, err := i.Underlying().MarshalJSON()
+	if err != nil {
+		fmt.Printf("marshaling output failed: %s", err.Error())
+		os.Exit(1)
+	}
+	if outputFile == "" {
+		fmt.Printf("writing to stdout\n")
+		os.Stdout.Write(j)
+	} else {
+		fmt.Printf("writing to %s\n", outputFile)
+		os.WriteFile(outputFile, j, 0644)
+	}
+}
