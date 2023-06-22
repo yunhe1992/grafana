d8a05a765ba3908d8a2ec85940db649bfb219017
joshhunt <josh@trtr.co>
2023-06-22 10:27:39 +0100

    clean up old husky hooks in .git/hooks path

--- scripts/cleanupHusky.mjs
+++ scripts/cleanupHusky.mjs
@@ -1,8 +1,30 @@
 /// @ts-check
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path';
 
 let changedHooksPath = false;
+
+const oldHuskyHooks = [
+  'applypatch-msg',
+  'commit-msg',
+  'post-applypatch',
+  'post-checkout',
+  'post-commit',
+  'post-merge',
+  'post-receive',
+  'post-rewrite',
+  'post-update',
+  'pre-applypatch',
+  'pre-auto-gc',
+  'pre-merge-commit',
+  'pre-push',
+  'pre-rebase',
+  'pre-receive',
+  'push-to-checkout',
+  'sendemail-validate',
+  'update',
+];
 
 //
 // Husky's postinstall script changes your local repo git config, so undo those changes
@@ -13,10 +35,30 @@ if (hooksConfig.stdout.trim() === '.husky') {
 }
 
 //
-// When user's first 'upgrade' to lefthook, lefthook will be installed to the old husky directory
+// When users first 'upgrade' to lefthook, lefthook will be installed to the old husky directory
 // so now we reinstall lefthook after changing the hooksPath in the previous step.
 if (changedHooksPath) {
-  childProcess.spawnSync('yarn', ['run', 'lefthook', 'install'], { stdio: 'inherit' });
+  childProcess.spawnSync('yarn', ['run', 'lefthook', 'install', '-f'], { stdio: 'inherit' });
+
+  //
+  // Previous version of husky also installed its hooks into .git/hooks which we now need to
+  // clean up. We'll just rename them to .old so nothing is lost permanently.
+  for (const hookFileName of oldHuskyHooks) {
+    const hookPath = path.join('.git', 'hooks', hookFileName);
+    try {
+      const hookContents = fs.readFileSync(hookPath).toString();
+      if (!hookContents.includes('husky')) {
+        continue;
+      }
+
+      fs.renameSync(hookPath, hookPath + '.old');
+    } catch (err) {
+      // Don't log an error if it's just the "file not exists" error
+      if (err.code !== 'ENOENT') {
+        console.error(`Error cleaning up old hook ${hookPath}: ${err.message ?? err} `);
+      }
+    }
+  }
 }
 
 //
